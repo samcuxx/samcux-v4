d9147f570e0239eb0a1c141a7a998de72a67b93d
samcuxx <samuellove228@gmail.com>
2024-10-18 17:30:09 +0000

    👌SamCux v5.2.3👌

--- components/ui/Heading1.tsx
+++ components/ui/Heading1.tsx
@@ -5,7 +5,7 @@ interface Heading1Props {
 
 export function Heading1({ children, className = '' }: Heading1Props) {
   return (
-    <h1 className={`scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-5xl ${className}`}>
+    <h1 className={`scroll-m-20 text-[2.5rem] sm:text-[2.5rem] md:text-4xl lg:text-5xl xl:text-6xl font-extrabold tracking-tight ${className}`}>
       {children}
     </h1>
   );
