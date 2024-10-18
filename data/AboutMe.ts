adfb2f43a7d2d0ae9fe3c926b1e9c144488a854b
samcuxx <samuellove228@gmail.com>
2024-10-18 17:26:14 +0000

    👌SamCux v5.2.2👌

--- data/AboutMe.ts
+++ data/AboutMe.ts
@@ -1,19 +1,16 @@
-
-
 export const aboutMe = {
     paragraphs: [
         {
-            text: "Hi, I'm Samuel Amoah, a third-year computer science student in Ghana. I'm also a <highlight>software developer</highlight>, <highlight>software developer</highlight>, and <highlight>software developer</highlight>. I'm passionate about using technology to solve real-world problems and create engaging experiences for users."
+            text: "I am a self-driven, career-oriented <highlight>software developer</highlight> specializing in front-end development and open-source, currently pursuing a <highlight>bachelor's degree in computer science</highlight>. My expertise lies in building interactive web applications on the client side. Primarily working with technologies like <highlight>JavaScript</highlight>, <highlight>Next.js</highlight>, <highlight>TypeScript</highlight> and <highlight>Python</highlight>."
         },
         {
-            text: "I have <highlight>5 years</highlight> of experience building and maintaining websites and web applications using <highlight>Next.js</highlight>, <highlight>React</highlight>, and <highlight>Tailwind CSS</highlight>. I'm also proficient in a variety of popular front-end frameworks and libraries, such as <highlight>Shacdn UI</highlight>, <highlight>Tailwind</highlight>, and <highlight>Typescript</highlight>."
+            text: "I strongly believe in continuous learning and improving myself, so I try my best to learn in any situation possible, unfavorable or not."
         },
         {
-            text: "In addition to my front-end development skills, I'm also a <highlight>content creator</highlight> and <highlight>graphic designer</highlight>. I have experience writing blog posts, articles, and social media content. I'm also proficient in using <highlight>Adobe Photoshop</highlight> and <highlight>Adobe Illustrator</highlight> to create graphics and logos."
+            text: "In addition to my front-end development skills, I'm also a skilled <highlight>content creator</highlight> and <highlight>graphic designer</highlight>. I have experience writing blog posts, articles, and social media content. I'm also proficient in using <highlight>Adobe Photoshop</highlight> and <highlight>Illustrator</highlight> to create graphics and logos."
         },
         {
             text: "I'm currently looking for opportunities to use my skills to make a positive impact on the world. I'm interested in working on projects that are related to <highlight>education</highlight>, <highlight>social justice</highlight>, and <highlight>environmental sustainability</highlight>."
         }
     ]
 }
-
